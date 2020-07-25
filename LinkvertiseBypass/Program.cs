7f2e612e87e7cc643ea2d6ccbd466c7fd54aff97
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-07-25 13:48:08 +0200

    Hahaha :(

---
+++ LinkvertiseBypass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Leaf.xNet;
+
+namespace LinkvertiseBypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put your linkvertise link here: ");
+            var link = Console.ReadLine();
+
+            using (HttpRequest bypass = new HttpRequest())
+            {
+                var resp = bypass.Post($"https://online-coding.eu/api/LinkvertiseBypass.php?url={link}");
+                Process.Start(resp.ToString());
+            }
+        }
+    }
+}
